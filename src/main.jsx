087a53c363cf8d513c0200cb557f2e0fee200e05
Valentina Lopez <valentinalopezh98@gmail.com>
2025-08-06 13:22:22 -0300

    fix CluesContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { WordContextProvider } from "./context/WordContext.jsx";
-import { CluesContextProvider } from "./context/cluesContext.jsx";
+import { CluesContextProvider } from "./context/CluesContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
